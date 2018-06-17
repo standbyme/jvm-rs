d0b534ffde57dd28e8f0f46828fe8dd57c29b21f
StandbyMe <stand-by-me@mail.com>
2018-06-17 14:00:23 +0800

    read byte from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let path: &str = "Object.class";
+    let input: File = File::open(path).unwrap();
+    let bytes = input.bytes().map(|x| x.unwrap());
+    for byte in bytes {
+        println!("{:X}", byte);
+    }
 }
