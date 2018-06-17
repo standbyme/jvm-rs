77525edea32eae9e5435a753e788d16afe0eb838
StandbyMe <stand-by-me@mail.com>
2018-06-17 21:28:20 +0800

    read and check magic

--- src/classfile.rs
+++ src/classfile.rs
@@ -1,10 +1,28 @@
+extern crate byteorder;
+
+use self::byteorder::{ByteOrder, BigEndian};
+
 pub trait ClassReader {
-    fn read_uint8(&self) -> (u8, &[u8]);
+    fn read_u8(&self) -> (u8, &[u8]);
+    fn read_u32(&self) -> (u32, &[u8]);
+    fn read_and_check_magic(&self) -> (u32, &[u8]);
 }
 
 impl ClassReader for [u8] {
-    fn read_uint8(&self) -> (u8, &[u8]) {
+    fn read_u8(&self) -> (u8, &[u8]) {
         let (a, b) = self.split_at(1);
         (a[0], b)
+    }
+
+    fn read_u32(&self) -> (u32, &[u8]) {
+        let (a, b) = self.split_at(4);
+        (BigEndian::read_u32(a), b)
+    }
+
+    fn read_and_check_magic(&self) -> (u32, &[u8]) {
+        let result = self.read_u32();
+        let (magic, _) = result;
+        assert_eq!(magic, 0xCAFEBABE);
+        result
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::fs::File;
-use std::io::prelude::*;
-
 mod classfile;
 
+use std::fs::File;
+use std::io::prelude::*;
 use classfile::ClassReader;
 
 fn main() {
     let path: &str = "Object.class";
     let input: File = File::open(path).unwrap();
     let bytes: Vec<u8> = input.bytes().map(|x| x.unwrap()).collect();
-    let (a, b) = bytes.read_uint8();
-    println!("{:?}", a);
-    let (c, d) = b.read_uint8();
-    println!("{:?}",c);
-    println!("{:?}",d);
-
+    let (_, after_magic) = bytes.read_and_check_magic();
+    println!("{:?}", after_magic);
 }
