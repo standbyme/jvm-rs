d54f0e2b5501917d335c68ec3073cae6ae77f142
StandbyMe <stand-by-me@mail.com>
2018-07-01 10:33:21 +0800

    finish basic instruction

--- src/instruction/comparison/if_icmp.rs
+++ src/instruction/comparison/if_icmp.rs
@@ -2,34 +2,31 @@ use util::code_reader::CodeReader;
 use rtda::frame::Frame;
 use instruction::instruction::ExecuteResult;
 
-//#[allow(non_camel_case_types)]
-//pub struct IF_ICMPGT {
-//    offset: isize
-//}
-//
-//impl Instruction for IF_ICMPGT {
-//    fn execute(&self, frame: Frame) -> ExecuteResult {
-//        let Frame { operand_stack, local_vars } = frame;
-//
-//
-////        func (self *IF_ICMPGT) Execute(frame *rtda.Frame) {
-////            if val1, val2 := _icmpPop(frame); val1 > val2 {
-////                base.Branch(frame, self.Offset)
-////            }
-////        }
-//        // todo
-//        let frame = Frame { operand_stack, local_vars };
-//
-//        ExecuteResult {
-//            frame,
-//            offset: 0,
-//        }
-//    }
-//}
-//
-//impl IF_ICMPGT {
-//    pub fn new(reader: CodeReader) -> (Box<dyn Instruction>, CodeReader) {
-//        let (offset, reader) = reader.read_i16();
-//        (Box::new(IF_ICMPGT { offset }), reader)
-//    }
-//}
+#[allow(non_snake_case)]
+fn _icmpPop(frame: Frame) -> (i32, i32, Frame) {
+    let Frame { operand_stack, local_vars } = frame;
+
+    let (val2, operand_stack) = operand_stack.pop_int();
+    let (val1, operand_stack) = operand_stack.pop_int();
+
+    let frame = Frame { operand_stack, local_vars };
+    (val1, val2, frame)
+}
+
+#[allow(non_snake_case)]
+pub fn IF_ICMPGT(reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+    let (offset, reader) = reader.read_i16();
+
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 > val2 {
+        offset as isize
+    } else {
+        0
+    };
+
+    let execute_result = ExecuteResult {
+        frame,
+        offset,
+    };
+    (execute_result, reader)
+}
--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -4,3 +4,4 @@ mod instruction;
 mod load;
 mod math;
 mod store;
+mod control;
