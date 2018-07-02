de0a73c1a6dc1fd45095485974642c8d5d878ee4
Swapnil Bhosale <sswapnil.1993@gmail.com>
2018-07-02 18:42:58 +0530

    add all icmp instructions

--- src/instruction/comparison/if_icmp.rs
+++ src/instruction/comparison/if_icmp.rs
@@ -30,3 +30,67 @@ pub fn IF_ICMPGT(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeR
     let execute_result = ExecuteResult { frame, offset };
     (execute_result, code_reader)
 }
+
+
+pub fn IF_ICMPGE(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+	println!("IF_ICMPGE");
+    let (offset, code_reader) = code_reader.read_i16();
+    
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 >= val2 { offset as isize } else { 0 };
+
+    let execute_result = ExecuteResult { frame, offset };
+    (execute_result, code_reader}
+
+}
+
+pub fn IF_ICMPEQ(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+	println!("IF_ICMPEQ");
+    let (offset, code_reader) = code_reader.read_i16();
+    
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 == val2 { offset as isize } else { 0 };
+
+    let execute_result = ExecuteResult { frame, offset };
+    (execute_result, code_reader}
+
+}
+
+
+pub fn IF_ICMPNE(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+	println!("IF_ICMPNE");
+    let (offset, code_reader) = code_reader.read_i16();
+    
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 != val2 { offset as isize } else { 0 };
+
+    let execute_result = ExecuteResult { frame, offset };
+    (execute_result, code_reader};
+
+}
+
+pub fn IF_ICMPLT(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+	println!("IF_ICMPLT");
+    let (offset, code_reader) = code_reader.read_i16();
+    
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 < val2 { offset as isize } else { 0 };
+
+    let execute_result = ExecuteResult { frame, offset };
+    (execute_result, code_reader}
+
+}
+
+pub fn IF_ICMPLE(code_reader: CodeReader, frame: Frame) -> (ExecuteResult, CodeReader) {
+	println!("IF_ICMPLE");
+    let (offset, code_reader) = code_reader.read_i16();
+    
+    let (val1, val2, frame) = _icmpPop(frame);
+    let offset = if val1 <= val2 { offset as isize } else { 0 };
+
+    let execute_result = ExecuteResult { frame, offset };
+    (execute_result, code_reader}
+
+}
+
+
